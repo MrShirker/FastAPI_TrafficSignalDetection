f00296519b7ac7c990338f323e006feb224b9a64
MrShirker <shirker97@gmail.com>
2023-05-21 21:46:57 +0200

    Añadido tracking

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ import random
 import ultralytics
 import os
 
+from sort import Sort
+
 app = FastAPI()
 
 app.mount("/gif", StaticFiles(directory="gif"), name="gif")
@@ -33,6 +35,9 @@ model_names = [file_name for file_name in file_names if os.path.splitext(file_na
 custom_model_dict = {model_name: None for model_name in model_names}
 
 colors = [tuple([random.randint(0, 255) for _ in range(3)]) for _ in range(100)] #for bbox plotting
+
+# Crear la instancia de SORT
+mt_tracker = Sort()
 
 ##############################################
 #-------------GET Request Routes--------------
@@ -66,7 +71,8 @@ async def root():
 def detect_via_api(request: Request,
                 file_list: List[UploadFile] = File(...), 
                 model_name: str = Form(...),
-                img_size: Optional[int] = Form(640)):
+                img_size: Optional[int] = Form(640),
+                tracking: Optional[bool] = Form(False)):
     
     '''
     Requires an image file upload, model name (ex. yolov5s). 
@@ -101,13 +107,41 @@ def detect_via_api(request: Request,
         if custom_model_dict[model_name] is None:
             custom_path = 'modelos/'+model_name
             custom_model_dict[model_name] = torch.hub.load('ultralytics/yolov5', 'custom',  path=custom_path)
+        
+        # Obtener las detecciones
         results = custom_model_dict[model_name](img_batch_rgb, size = img_size) 
         json_results = results_to_json(results,custom_model_dict[model_name])
+        
+        if tracking:
+            detections = results.pred[0].numpy()
+            # Actualizar SORT
+            track_bbs_ids = mt_tracker.update(detections)
+            
+            if len(track_bbs_ids) > 0:
+                for j in range(len(track_bbs_ids.tolist())):
+                    coords = track_bbs_ids.tolist()[j]
+                    
+                    # Agregar el ID actualizado a json_results
+                    json_results[0][j]['tracker_id'] = int(coords[4])  
     elif yolo_model:
         if model_dict[model_name] is None:
             model_dict[model_name] = torch.hub.load('ultralytics/yolov5', model_name, pretrained=True)
+        
+        # Obtener las detecciones
         results = model_dict[model_name](img_batch_rgb, size = img_size) 
         json_results = results_to_json(results,model_dict[model_name])
+        
+        if tracking:
+            detections = results.pred[0].numpy()
+            # Actualizar SORT
+            track_bbs_ids = mt_tracker.update(detections)
+            
+            if len(track_bbs_ids) > 0:
+                for j in range(len(track_bbs_ids.tolist())):
+                    coords = track_bbs_ids.tolist()[j]
+                    
+                    # Agregar el ID actualizado a json_results
+                    json_results[0][j]['tracker_id'] = int(coords[4])     
     else:
         print("El modelo elegido no esta disponible")
 
